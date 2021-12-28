c2c74891ed04872495431c4d2ce3aabc3a922d76
zoujinde <95668087+zoujinde@users.noreply.github.com>
2021-12-28 00:56:47 -0500

    Delete QuizWebApp/src/main/java/com/jinde/web/QuizWebApp directory

--- QuizWebApp/src/main/java/com/jinde/web/QuizWebApp/web.bat
+++
@@ -1,4 +0,0 @@
-call mvn clean package
-rmdir /S /Q D:\data\web\apache-tomcat-9.0.56\webapps\hello
-del D:\data\web\apache-tomcat-9.0.56\webapps\hello.war
-copy target\hello.war D:\data\web\apache-tomcat-9.0.56\webapps\hello.war
