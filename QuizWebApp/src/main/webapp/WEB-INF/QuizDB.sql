aa877b1361723f96347f6b3092474f55a91d1181
zoujinde <zoujinde@gmail.com>
2022-01-06 22:06:18 -0500

    Update QuizDB.sql

--- QuizWebApp/src/main/webapp/WEB-INF/QuizDB.sql
+++ QuizWebApp/src/main/webapp/WEB-INF/QuizDB.sql
@@ -53,24 +53,24 @@ CREATE TABLE quiz_result (
 -- Insert test data
 insert into user values(1, 'Admin', 'pass', '', '', '', '', '2022-01-01');
 insert into quiz values(1, 'Quiz 2022', '2022-01-01');
-insert into quiz_item values(1, 1, 'Question 1 : Are you in US?', '(a) Yes || (b) No', 0);
+insert into quiz_item values(1, 1, 'Question 1 : Are you in US?', '(a) Yes # (b) No', 0);
 insert into quiz_result values(1, 1, 1, '0', '2022-01-01');
 -- Insert quiz 2
 insert into quiz values(2, 'Survey of Spanish Media', '2022-01-01');
 insert into quiz_item values(2, 1, 'To what extent is your knowledge on the Spanish media?',
-       '(a) A huge extent || (b) Quite a huge extent || (c) An average extent || (d) Quite a limited extent || (e) A limited extent || (f) Not sure',
+       '(a) A huge extent # (b) Quite a huge extent # (c) An average extent # (d) Quite a limited extent # (e) A limited extent # (f) Not sure',
        0);
 insert into quiz_item values(2, 2, 'Through which media, the Spanish or English, do you most frequently retrieve the most updated news?',
-       '(a) Spanish media || (b) English media || (c) Both || (d) Not sure',
+       '(a) Spanish media # (b) English media # (c) Both # (d) Not sure',
        0);
 insert into quiz_item values(2, 3, 'Through which channel of the Spanish media do you most frequently retrieve the latest news?',
-       '(a) Mobile Phone || (b) Newspapers || (c) Television (Newscasts) || (d) Computer (Internet, webpages, blogs, etc) || (e) Radio/radio stations (FM 88.3, 93.3, 95.8, 100.3, etc)',
+       '(a) Mobile Phone # (b) Newspapers # (c) Television (Newscasts) # (d) Computer (Internet, webpages, blogs, etc) # (e) Radio/radio stations (FM 88.3, 93.3, 95.8, 100.3, etc)',
        1);
 insert into quiz_item values(2, 4, 'Through which channel of the English media do you most frequently retrieve the latest news?',
-       '(a) Handphone || (b) Newspapers (The Straits Times, Today, The Newpaper, etc) || (c) Television (Newscasts) || (d) Computer (Internet, webpages, blogs, etc) || (e) Radio/radio stations (FM 91.3, 98.7, etc)',
+       '(a) Handphone # (b) Newspapers (The Straits Times, Today, The Newpaper, etc) # (c) Television (Newscasts) # (d) Computer (Internet, webpages, blogs, etc) # (e) Radio/radio stations (FM 91.3, 98.7, etc)',
        1);
 insert into quiz_item values(2, 5, 'In your opinion, which topic has the Spanish media been stressing on?',
-       '(a) Gossipy news || (b) Information and technology || (c) Violence cases || (d) Sports || (e) Politics',
+       '(a) Gossipy news # (b) Information and technology # (c) Violence cases # (d) Sports # (e) Politics',
        1);
 commit;
 
