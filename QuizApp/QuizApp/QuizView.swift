7a65b432e1efab775bf3a1ef3d5b40e16801ed53
zoujinde <95668087+zoujinde@users.noreply.github.com>
2022-06-15 21:02:54 -0700

    Add WebView

--- QuizApp/QuizApp/QuizView.swift
+++ QuizApp/QuizApp/QuizView.swift
@@ -7,14 +7,49 @@
 //
 
 import SwiftUI
-import Foundation
+import WebKit
 
 struct QuizView: View {
+
+    private var mWebView = WebView()
 
     // init
     init () {
         let gitUrl = URL(string: "https://raw.githubusercontent.com/zoujinde/zoujinde.github.io/main/server.txt")
-        var request = URLRequest(url:gitUrl!)
+        mWebView.load(gitUrl: gitUrl!)
+    }
+
+    // body
+    var body: some View {
+        VStack {
+            //Text("Purple Sense App Is Loading ...")
+            mWebView
+        }
+    }
+}
+
+// WebView
+struct WebView: UIViewRepresentable {
+
+    private var mWKWebView = WKWebView()
+    private var mDelegate = Delegate()
+
+    func makeUIView(context: Context) -> WKWebView {
+        mWKWebView.uiDelegate = mDelegate
+        return mWKWebView
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        print("updateUIView : \(webView)")
+        /* http://quizwebapp-env.eba-dhiyby8z.us-east-2.elasticbeanstalk.com/sign-in.jsp
+        let url = URL(string:"https://www.google.com")
+        let request = URLRequest(url:url!)
+        webView.load(request)
+         */
+    }
+
+    func load(gitUrl:URL) {
+        var request = URLRequest(url: gitUrl)
         request.httpMethod = "GET"
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let err = error {
@@ -27,14 +62,30 @@ struct QuizView: View {
             if let data = data, let urlStr = String(data:data, encoding: .utf8) {
                 print("init url : \(urlStr)")
                 if let url = URL(string:urlStr)  {
-                    UIApplication.shared.open(url, options:[:], completionHandler:(nil))
+                    // USE `weakSelf?` INSTEAD OF `self` to avoid build error
+                    let request = URLRequest(url:url)
+                    //UIApplication.shared.open(url, options:[:], completionHandler:(nil))
+                    self.mWKWebView.load(request)
                 }
             }
         }
         task.resume()
     }
+}
 
-    var body: some View {
-        Text("Purple Sense App Is Loading ...")
+//https://github.com/LINGLemon/LXFSwiftApp/tree/master/LXFSwiftApp/Pages/WKWebView
+class Delegate : NSObject, WKUIDelegate{
+    // show js alert
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage msg: String,
+                 initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        if let rootVC = UIApplication.shared.keyWindow?.rootViewController {
+            let alert = UIAlertController.init(title: msg, message: nil, preferredStyle: .alert)
+            let action = UIAlertAction.init(title: "OK", style: .default) { (action) in
+                completionHandler()
+            }
+            alert.addAction(action)
+            //self.present(alert, animated: true, completion: nil)
+            rootVC.present(alert, animated: true)
+        }
     }
 }
