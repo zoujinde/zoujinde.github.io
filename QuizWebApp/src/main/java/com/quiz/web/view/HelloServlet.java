46c82150fd6cfcb986f7546b97a74b41b1fb8653
zoujinde <zoujinde@gmail.com>
2022-08-14 11:13:32 -0400

    Modify web.xml

--- QuizWebApp/src/main/java/com/quiz/web/view/HelloServlet.java
+++ QuizWebApp/src/main/java/com/quiz/web/view/HelloServlet.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/")
+// Because UserFilter uses : "/*" to block other resources
+// So we remark below line : "/"
+// @WebServlet(urlPatterns = "/")
 public class HelloServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
